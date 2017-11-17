feb4a649c6a4e63e3fc224c0affa2038c194f7b8
mfieldsboston <mfields11@gmail.com>
2017-11-17 17:10:38 -0500

    updated appjs and appcss

---
+++ src/Components/App - Copy/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+//import './Components/App/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <div className="App-playlist">
+          </div>
+        </div>
+    </div>
+    );
+  }
+}
+
+export default App;
